e6e4c8ceef0f166546be8417214c54916cb05716
S1D <83898164+lolkaXD@users.noreply.github.com>
2022-11-02 23:18:28 +0530

    Add files via upload

---
+++ system/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define('app_name', 'AnonTip'); 
+define('app_desc', 'Upload Evidence Care Free'); 
+
+
+define('FILELIST', "jpeg,jpg,gif,png,zip,xls,doc,mp3,mp4,mpeg,wav,avi,rar,7z,txt,pdf,docs,docx"); 
+define('size_verification', true); 
+define('max_size', '8000000000');
+define('min_size', '0'); 
+define('file_destination', 'files');
+define('file_url_destination', 'https://anontip.000webhostapp.com'); 
